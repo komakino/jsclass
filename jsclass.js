867e19a620922c403a6a61576cfc73ec9d1e7a27
Salem Sallnäs <salem.sallnas@gmail.com>
2015-08-25 23:48:47 +0200

    refactoring

--- jsclass.js
+++ jsclass.js
@@ -1,15 +1,17 @@
 function Class(fn,prototype,descriptors){
+    function _merge(a,b){ for(v in b) b.hasOwnProperty(v) && (a[v] = b[v])}
+
     fn.extends = function(parent){
         function temp() { this.constructor = fn; }
         temp.prototype = parent.prototype;
-        for(p in fn.prototype) fn.prototype.hasOwnProperty(p) && (temp.prototype[p] = fn.prototype[p]);
+        _merge(temp.prototype,fn.prototype)
         fn.prototype = new temp;
         fn.__super = parent.prototype
 
         return fn;
     }
     
-    for(p in prototype) prototype.hasOwnProperty(p) && (fn.prototype[p] = prototype[p]);
+    _merge(fn.prototype,prototype);
     descriptors && Object.defineProperties(fn.prototype,descriptors);
 
     return fn;
